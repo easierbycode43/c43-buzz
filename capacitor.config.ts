5068ce5dfd3c27e3064872b46c153bc40881d127
EasierByCode[43] <daniel@community43.org>
2021-10-12 11:36:20 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'C43 Buzz',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
